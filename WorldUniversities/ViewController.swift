6ca423c53afd5079efe5ba4bde0c99ddbaadd3c4
Dima Monid <1234567waka.waka@gmail.com>
2025-03-06 16:22:16 +0300

    Initial Commit

---
+++ WorldUniversities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldUniversities
+//
+//  Created by Дима Монид on 6.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
